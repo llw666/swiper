565f78b8cbddf29d616d035c1aef9197fc7c5d0e
zx <llw@2019.730>
2019-11-04 23:36:40 +0800

    创建了user模型

--- user/models.py
+++ user/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    SEX = (
+        ('male', '男性'),
+        ('female', '女性')
+    )
+    LOCATION = (
+        ('北京', '北京'),
+        ('上海', '上海'),
+        ('广东', '广东'),
+        ('南京', '南京'),
+        ('重庆', '重庆'),
+        ('西安', '西安'),
+        ('武汉', '武汉'),
+        ('安徽', '长沙'),
+    )
+    phonenum = models.CharField(max_length=20, unique=True, verbose_name='手机号')
+    nickname = models.CharField(max_length=32, verbose_name='昵称')
+    sex = models.CharField(max_length=8, choices=SEX, default='male', verbose_name='性别')
+    birthday = models.DateField(default='1990-1-1', verbose_name='出生日')
+    avatar = models.CharField(max_length=256, verbose_name='个人形象')
+    location = models.CharField(max_length=16, choices=LOCATION, default='北京', verbose_name='常居地')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'phonenum': self.phonenum,
+            'nickname': self.nickname,
+            'sex': self.sex,
+            'birthday': str(self.birthday),
+            'avatar': self.avatar,
+            'location': self.location,
+        }
