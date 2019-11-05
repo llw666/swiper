5a913738ae5d51db17d2b490563cf01eeb0f2ecb
zx <llw@2019.730>
2019-11-05 21:11:17 +0800

    获取验证码接口

--- user/apis.py
+++ user/apis.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from user.logics import send_vcode
+
+def get_vcode(request):
+    '''用户获取验证码接口'''
+    phonenum = request.GET.get('phonenum')
+    result = send_vcode(phonenum)
+    if result:
+        return JsonResponse({'code': 0, 'data': None})
+    else:
+        return JsonResponse({'code': 0, 'data': None})
+
+
+def submit_vcode(request):
+    '''用户提交验证接口'''
+    pass
